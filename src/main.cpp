168fde69c0763bf104ce8a16955b7911f656b792
Asher Broadhead <asherb1@optusnet.com.au>
2025-03-15 18:24:32 +1100

    add some global definitions

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,9 @@
 #define SERVO1_PIN 10
 #define ZERO_OFFSET 90
 #define BUTTON_PIN 9
+#define MINIMUM_SERVO_TRAVEL -30
+#define MAXIMUM_SERVO_TRAVEL 30
+#define POT1_PIN A0
 
 Servo servo1;
 
@@ -17,7 +20,7 @@ void setup() {
 
 void loop() {
   servo1.write(servo_angle + ZERO_OFFSET);
-  int desired_angle = map(analogRead(A0), 0, 1023, -30, 30);
+  int desired_angle = map(analogRead(POT1_PIN), 0, 1023, MINIMUM_SERVO_TRAVEL, MAXIMUM_SERVO_TRAVEL);
   Serial.println(desired_angle);
   if (abs(servo_angle - desired_angle) > ANGLE_TOLERANCE){
      servo_angle = desired_angle;
